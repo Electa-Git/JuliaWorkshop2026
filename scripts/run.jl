69cd2762ab71e4b1daa6dba281add37a33790f02
Cas Bex <cas.bex@kuleuven.be>
2026-02-19 17:00:23 +0100

    Use Dict{Symbol, Any} to represent config, and benchmark

--- scripts/run.jl
+++ scripts/run.jl
@@ -2,25 +2,53 @@ import CSV
 import Dates
 using BenchmarkTools
 import YAML
+import ArgParse
+
+### * Parse arguments
+function parse_args()
+    s = ArgParse.ArgParseSettings()
+    ArgParse.@add_arg_table s begin
+        "--output", "-o"
+        help = "output file"
+        arg_type = String
+        default = "result.csv"
+        "system"
+        help = "system config file"
+        required = true
+    end
+    if isinteractive()
+        Dict("output" => "result.csv", "system" => "system.yaml")
+    else
+        s
+    end
+end
 
 ### * Config
-output_file = "result.csv"
-config_file = "system.yaml"
-data = YAML.load_file(config_file)
+args = parse_args()
+output_file = args["output"]
+config_file = args["system"]
+data = try
+    YAML.load_file(config_file, dicttype = Dict{Symbol, Float64})
+catch err
+    # cannot parse everything as a Dict{Symbol, Float64}
+    # this is the case since we use nested dictionaries
+    YAML.load_file(config_file, dicttype = Dict{Symbol, Any})
+end
 
 ## ** Time
+tstart, tend, tstep = (; data[:time]...)
 
 ### *** Calculate machine constant from nominal characteristics
-let d = data["motor"]
-    n_nom, Pme, I_a_nom, I_b_nom = d["n_nom"], d["Pme"], d["I_a_nom"], d["I_b_nom"]
+let d = data[:motor]
+    U_b, n_nom, Pme, I_a_nom, I_b_nom = (; d...)
     ω_nom = n_nom * 2 * pi / 60   # rad/s
     T_nom = Pme / ω_nom           # Nm
     k = T_nom / I_a_nom / I_b_nom # Nm/A^2
     R_b = U_b / I_b_nom           # Ω
-    d["ω_nom"] = ω_nom
-    d["T_nom"] = T_nom
-    d["k"] = k
-    d["R_b"] = R_b
+    d[:ω_nom] = ω_nom
+    d[:T_nom] = T_nom
+    d[:k] = k
+    d[:R_b] = R_b
 end
 
 ### ** Load
@@ -61,9 +89,52 @@ function run_loop(; tstart, tend, tstep, U_a, U_b, B, J, L_a, L_b, R_a, R_b, k, 
     return (ω_vec, i_a_vec, i_b_vec)
 end
 
-flat_data = merge((Dict(Symbol(vk) => Float64(vv) for (vk, vv) in v) for v in values(data))...)
+function run_loop_from_dict(d)
+    (; tstart, tend, tstep, U_a, U_b, B, J, L_a, L_b, R_a, R_b, k, T_nom) = (; d...)
 
-@benchmark run_loop(; flat_data...)
+    trange = range(start = tstart, stop = tend, step = tstep)
+
+    ω_vec = zeros(length(trange))
+    i_a_vec = zeros(length(trange))
+    i_b_vec = zeros(length(trange))
+
+    ### * Simulate
+
+    for (idx, t) in enumerate(trange[begin:(end - 1)])
+        ω = ω_vec[idx]
+        i_a = i_a_vec[idx]
+        i_b = i_b_vec[idx]
+        e = k * i_b * ω
+        Tm = k * i_a * i_b
+
+        dω = (-B * ω - T_u(t, T_nom) + Tm) / J
+        di_a = (U_a - e - R_a * i_a) / L_a
+        di_b = (U_b - R_b * i_b) / L_b
+
+        # Forward Euler x(t+dt) = x(t) + dt * dx/dt (t)
+        ω_new = ω + dω * tstep
+        i_a_new = i_a + di_a * tstep
+        i_b_new = i_b + di_b * tstep
+
+        ω_vec[idx + 1] = ω_new
+        i_a_vec[idx + 1] = i_a_new
+        i_b_vec[idx + 1] = i_b_new
+    end
+    return (ω_vec, i_a_vec, i_b_vec)
+end
+
+flat_data = reduce(merge, values(data))
+
+# Pay attention to typing
+println("pass data as keyword arguments")
+display(@benchmark run_loop(; flat_data...))
+
+println("\npass data as Dict{Symbol, Any}")
+display(@benchmark run_loop_from_dict(flat_data))
+
+println("\npass data as a a NamedTuple")
+display(@benchmark run_loop_from_dict((; flat_data...)))
+
 (ω_vec, i_a_vec, i_b_vec) = run_loop(; flat_data...)
 
 ### * Save result
