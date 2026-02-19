9380122ef8fb91f6777c57fe38a04a04b41110f8
Cas Bex <cas.bex@kuleuven.be>
2026-02-19 16:31:43 +0100

    Configure script with yaml file

--- scripts/run.jl
+++ scripts/run.jl
@@ -1,40 +1,29 @@
 import CSV
 import Dates
 using BenchmarkTools
+import YAML
 
 ### * Config
 output_file = "result.csv"
+config_file = "system.yaml"
+data = YAML.load_file(config_file)
 
 ## ** Time
-tstart = 0.0   # s
-tend = 5.0     # s
-tstep = 0.0001 # s
-
-### * Constants
-### ** Motor nominal characteristics
-# these characteristics have been taken from
-# Elektrische Energie en Aandrijvingen Exercise Session 3 Exercise 1
-Pme = 18.9e3  # W
-U_a = 470     # V
-U_b = 170     # V
-I_a_nom = 49  # A
-I_b_nom = 4.9 # A
-n_nom = 1285  # rpm
-R_a = 1.46    # Ω
-
-# these characteristics have been made up
-L_b = 1.0e-1 # H
-L_a = 1.0e-1 # H
 
 ### *** Calculate machine constant from nominal characteristics
-ω_nom = n_nom * 2 * pi / 60   # rad/s
-T_nom = Pme / ω_nom           # Nm
-k = T_nom / I_a_nom / I_b_nom # Nm/A^2
-R_b = U_b / I_b_nom           # Ω
+let d = data["motor"]
+    n_nom, Pme, I_a_nom, I_b_nom = d["n_nom"], d["Pme"], d["I_a_nom"], d["I_b_nom"]
+    ω_nom = n_nom * 2 * pi / 60   # rad/s
+    T_nom = Pme / ω_nom           # Nm
+    k = T_nom / I_a_nom / I_b_nom # Nm/A^2
+    R_b = U_b / I_b_nom           # Ω
+    d["ω_nom"] = ω_nom
+    d["T_nom"] = T_nom
+    d["k"] = k
+    d["R_b"] = R_b
+end
 
 ### ** Load
-J = 1.0                                         # kgm^2
-B = 1.0                                         # kgm^2/s
 T_u(t, T_nom) = T_nom * (0.5 + 0.25 * sin(2 * pi * t)) # Nm
 
 ### * Initialize
@@ -72,8 +61,10 @@ function run_loop(; tstart, tend, tstep, U_a, U_b, B, J, L_a, L_b, R_a, R_b, k, 
     return (ω_vec, i_a_vec, i_b_vec)
 end
 
-@benchmark run_loop(; tstart, tend, tstep, U_a, U_b, B, J, L_a, L_b, R_a, R_b, k, T_nom)
-(ω_vec, i_a_vec, i_b_vec) = run_loop(; tstart, tend, tstep, U_a, U_b, B, J, L_a, L_b, R_a, R_b, k, T_nom)
+flat_data = merge((Dict(Symbol(vk) => Float64(vv) for (vk, vv) in v) for v in values(data))...)
+
+@benchmark run_loop(; flat_data...)
+(ω_vec, i_a_vec, i_b_vec) = run_loop(; flat_data...)
 
 ### * Save result
 time = range(tstart, step = tstep, length = length(ω_vec)) # avoid off-by-one errors
