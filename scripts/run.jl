088170c451b779537a19119c621724d4226aea8e
Cas Bex <cas.bex@kuleuven.be>
2026-05-01 14:41:35 +0200

    DC motor model

--- scripts/run.jl
+++ scripts/run.jl
@@ -3,11 +3,16 @@ import Dates
 using BenchmarkTools
 import YAML
 import ArgParse
+import Revise
+Revise.includet("../src/dcmotor.jl")
+using .DCMotor
 
 ### * Parse arguments
 function parse_cmd()
     s = ArgParse.ArgParseSettings()
     ArgParse.@add_arg_table s begin
+        "--benchmark", "-b"
+        action = :store_true
         "--output", "-o"
         help = "output file"
         arg_type = String
@@ -17,7 +22,7 @@ function parse_cmd()
         required = true
     end
     if isinteractive()
-        Dict("output" => "result.csv", "system" => "system.yaml")
+        Dict("output" => "result.csv", "system" => "system_parallel.yaml", "benchmark" => true)
     else
         ArgParse.parse_args(s)
     end
@@ -43,12 +48,21 @@ let d = data[:motor]
     (; U_b, n_nom, Pme, I_a_nom, I_b_nom) = (; d...)
     ω_nom = n_nom * 2 * pi / 60   # rad/s
     T_nom = Pme / ω_nom           # Nm
-    k = T_nom / I_a_nom / I_b_nom # Nm/A^2
     R_b = U_b / I_b_nom           # Ω
-    d[:ω_nom] = ω_nom
     d[:T_nom] = T_nom
-    d[:k] = k
-    d[:R_b] = R_b
+    anchor = Anchor(; R = d[:R_a], L = d[:L_a], I_a_nom, I_b_nom, Pme, n_nom)
+    winding = ExcitationWinding(R_b, d[:L_b])
+    setupkey = get(d, :setup, "independent")
+    excitation = if setupkey == "independent"
+        Independent(winding)
+    elseif setupkey == "parallel"
+        Parallel(winding)
+    elseif setupkey == "series"
+        Series(winding)
+    else
+        throw(KeyError(setupkey))
+    end
+    d[:motor] = DCMotorWithWinding(anchor, excitation)
 end
 
 ### ** Load
@@ -57,7 +71,7 @@ T_u(t, T_nom) = T_nom * (0.5 + 0.25 * sin(2 * pi * t)) # Nm
 ### * Initialize
 
 # start of timing for benchmarking purposes
-function run_loop(; tstart, tend, tstep, U_a, U_b, B, J, L_a, L_b, R_a, R_b, k, T_nom, kwargs...)
+function run_loop(; tstart, tend, tstep, motor, T_nom, B, J, U_a, U_b, kwargs...)
     trange = range(start = tstart, stop = tend, step = tstep)
 
     ω_vec = zeros(length(trange))
@@ -70,12 +84,10 @@ function run_loop(; tstart, tend, tstep, U_a, U_b, B, J, L_a, L_b, R_a, R_b, k, 
         ω = ω_vec[idx]
         i_a = i_a_vec[idx]
         i_b = i_b_vec[idx]
-        e = k * i_b * ω
-        Tm = k * i_a * i_b
 
+        Tm = torque(motor, i_a, i_b)
         dω = (-B * ω - T_u(t, T_nom) + Tm) / J
-        di_a = (U_a - e - R_a * i_a) / L_a
-        di_b = (U_b - R_b * i_b) / L_b
+        di_a, di_b = derivative(motor, U_a, U_b, i_a, i_b, ω)
 
         # Forward Euler x(t+dt) = x(t) + dt * dx/dt (t)
         ω_new = ω + dω * tstep
@@ -88,52 +100,12 @@ function run_loop(; tstart, tend, tstep, U_a, U_b, B, J, L_a, L_b, R_a, R_b, k, 
     end
     return (ω_vec, i_a_vec, i_b_vec)
 end
-
-function run_loop_from_dict(d)
-    (; tstart, tend, tstep, U_a, U_b, B, J, L_a, L_b, R_a, R_b, k, T_nom) = (; d...)
-
-    trange = range(start = tstart, stop = tend, step = tstep)
-
-    ω_vec = zeros(length(trange))
-    i_a_vec = zeros(length(trange))
-    i_b_vec = zeros(length(trange))
-
-    ### * Simulate
-
-    for (idx, t) in enumerate(trange[begin:(end - 1)])
-        ω = ω_vec[idx]
-        i_a = i_a_vec[idx]
-        i_b = i_b_vec[idx]
-        e = k * i_b * ω
-        Tm = k * i_a * i_b
-
-        dω = (-B * ω - T_u(t, T_nom) + Tm) / J
-        di_a = (U_a - e - R_a * i_a) / L_a
-        di_b = (U_b - R_b * i_b) / L_b
-
-        # Forward Euler x(t+dt) = x(t) + dt * dx/dt (t)
-        ω_new = ω + dω * tstep
-        i_a_new = i_a + di_a * tstep
-        i_b_new = i_b + di_b * tstep
-
-        ω_vec[idx + 1] = ω_new
-        i_a_vec[idx + 1] = i_a_new
-        i_b_vec[idx + 1] = i_b_new
-    end
-    return (ω_vec, i_a_vec, i_b_vec)
-end
-
 flat_data = reduce(merge, values(data))
 
-# Pay attention to typing
-println("pass data as keyword arguments")
-display(@benchmark run_loop(; flat_data...))
-
-println("\npass data as Dict{Symbol, Any}")
-display(@benchmark run_loop_from_dict(flat_data))
-
-println("\npass data as a a NamedTuple")
-display(@benchmark run_loop_from_dict((; flat_data...)))
+if args["benchmark"]
+    println("pass data as keyword arguments")
+    display(@benchmark run_loop(; flat_data...))
+end
 
 (ω_vec, i_a_vec, i_b_vec) = run_loop(; flat_data...)
 
